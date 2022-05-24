b0b3e6dbe154609b7b4a47a61ac57d54be7f171b
Jalexandermurallas <jalexandermurallas@uts.edu.co>
2022-05-23 23:16:31 -0500

    backend-funcional
    primer backend funcional

---
+++ Backend/models/PcModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const PcTemplates = new Schema({
+    serialnumber:{
+        type:String,
+        required:true
+
+    },
+    brand:{
+        type:String,
+        required:true
+    },
+    model:{
+        type:String,
+        required:true
+    },
+    systemtype:{
+        type:String,
+        required:true
+    },
+    cpu:{
+        type:String,
+        required:true
+        
+    }
+
+})
+
+//export default model('PcDocument', PcTemplates);
+module.exports = PcTemplates;
---
+++ Backend/routes/routes.js
@@ -0,0 +1,27 @@
+const { response } = require("express")
+
+const express = require('express')
+const router = express.Router()
+const PcTemplatesCopy = require('../models/PcModel')
+
+router.post('/signup', (request, response) => {
+    const pcInfo = new PcTemplatesCopy({
+        
+        serialnumber:request.body.serialnumber,
+        brand:request.body.brand,
+        model:request.body.model,
+        systemtype:request.body.systemtype,
+        cpu:request.body.cpu,
+        
+
+    })
+    pcInfo.save()
+    .then(data =>{
+        response.json(data)
+    })
+    .catch(error =>{
+        response.json(error)
+    })
+} )
+
+module.exports = router 
