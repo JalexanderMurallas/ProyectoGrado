c30abcf917e7d97d62e5e279614ca6521899ea99
Parra14 <91274355+Parra14@users.noreply.github.com>
2022-10-24 15:22:17 -0500

    .

---
+++ Frontend/src/app/components/casos/list-casos/list-casos.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CasosServicesService } from 'src/app/services/casos-services.service';
+import { Casos } from 'src/app/models/casos';
+
+
+@Component({
+  selector: 'app-list-casos',
+  templateUrl: './list-casos.component.html',
+  styleUrls: ['./list-casos.component.css']
+})
+export class ListCasosComponent implements OnInit {
+  listCasos: Casos[] = [];
+
+  casosForm: FormGroup;
+  titulo = 'Crear Caso';
+
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService,
+    private _casosServicesService: CasosServicesService,
+    private aRouter: ActivatedRoute) {
+
+      this.casosForm = this.fb.group({
+
+        codigo: [''],
+        titulo: ['', Validators.required],
+        incidencia: [],
+        estado: [''],
+        
+      })
+
+     }
+
+  ngOnInit(): void {
+    this.obtenerCasos();
+  }
+
+  obtenerCasos() {
+    this._casosServicesService.getCasos().subscribe(data => {
+      console.log(data);
+      this.listCasos = data;
+    }, error => {
+      console.log(error);
+    })
+
+  }
+
+  eliminarCaso(id: any){
+    console.log(id);
+  }
+
+  agregarCaso() {
+
+    const caso: Casos = {
+      codigo: '',
+      titulo: this.casosForm.get('titulo')?.value,
+      incidencia: [],
+      estado: 'Abierto',
+    }
+
+    console.log(caso);
+    this._casosServicesService.guardarCaso(caso).subscribe(data => {
+      this.toastr.success('Caso guardado con Exito!!', 'Caso registrado!');
+    }, error => {
+      console.log(error);
+      this.casosForm.reset();
+    })
+  }
+
+}
